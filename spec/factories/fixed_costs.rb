0e95f2218d4a05dc3bacadce911f17aa6454258f
masugata1995 <masugata1995@gmail.com>
2021-11-07 20:46:10 +0900

    fixed_costモデルの単体テストを作成

--- spec/factories/fixed_costs.rb
+++ spec/factories/fixed_costs.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :fixed_cost do
-    name { "MyString" }
-    price { 1 }
-    scheduled_date { 1 }
-    note { "MyString" }
-    user_id { "MyString" }
-    category { nil }
-    payment_method { nil }
+    name { "example" }
+    price { rand(100..100000) }
+    scheduled_date { rand(1..31) }
+    note { "example" }
+    sequence(:user_id) { |i| "user_id#{i}" }
+    association :category
+    association :payment_method
   end
 end
