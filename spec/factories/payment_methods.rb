e7fc42e27eae340efa040617364304a5cb29ac42
masugata1995 <masugata1995@gmail.com>
2021-11-07 19:17:09 +0900

    payment_methodモデルの単体テスト作成

--- spec/factories/payment_methods.rb
+++ spec/factories/payment_methods.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :payment_method do
-    name { "MyString" }
+    sequence(:name) { |i| "payment_method#{i}" }
     income { false }
-    user_id { "MyString" }
+    sequence(:user_id) { |i| "user_id#{i}" }
   end
 end
